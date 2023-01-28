8bec0391e48b4b983c04870d465f6e674e8ee855
xAuroria <116680469+xAuroria@users.noreply.github.com>
2023-01-28 13:29:13 -0800

    Moments.js setup

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+// Timezone 1
+document.querySelector("#date-1").innerHTML = moment().format("MMMM Do YYYY");
+document.querySelector("#time-1").innerHTML = moment().format("h:mm:ss ");
+document.querySelector("#time-unit-1").innerHTML = moment().format("A");
+
+//Timezone 2
+document.querySelector("#date-2").innerHTML = moment().format("MMMM Do YYYY");
+document.querySelector("#time-2").innerHTML = moment().format("h:mm:ss ");
+document.querySelector("#time-unit-2").innerHTML = moment().format("A");
+
+//Timezone 3
+document.querySelector("#date-3").innerHTML = moment().format("MMMM Do YYYY");
+document.querySelector("#time-3").innerHTML = moment().format("h:mm:ss ");
+document.querySelector("#time-unit-3").innerHTML = moment().format("A");
+
+//Timezone 4
+document.querySelector("#date-4").innerHTML = moment().format("MMMM Do YYYY");
+document.querySelector("#time-4").innerHTML = moment().format("h:mm:ss ");
+document.querySelector("#time-unit-4").innerHTML = moment().format("A");
